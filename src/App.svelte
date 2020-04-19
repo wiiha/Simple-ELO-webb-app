37c632b4760f9e04ccc1a8823683e94fec2ddda9
Wilhelm Hansson <wille.live@gmail.com>
2020-04-19 22:42:57 +0200

    started to design basic layout of app

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,26 @@
 <script>
- import "../node_modules/bulma/css/bulma.min.css"
- 
- console.log("Running");
+  import "../node_modules/bulma/css/bulma.min.css";
+  import { dogs } from "./db.js";
+  import { randomIntFromInterval } from "./utils.js";
+
+  import DisplayCard from "./DisplayCard.svelte";
+
+  let competitors = dogs;
+  let testDog = dogs[0];
+
+  console.log("Running");
+  console.log("competitors", competitors);
+  
+
 </script>
 
-
-<h1>Hello World!</h1>
+<section class="section">
+  <div class="container">
+    <div class="columns">
+      <div class="column">
+        <DisplayCard dog={testDog} />
+      </div>
+      <div class="column">Second column</div>
+    </div>
+  </div>
+</section>
---
+++ src/DisplayCard.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let dog;
+  let src = dog.url;
+  let id = dog.id;
+</script>
+
+<div class="box has-text-centered">
+<p>ID: {id}</p>
+  <figure class="image">
+    <img {src} alt="picture of dog">
+  </figure>
+</div>
