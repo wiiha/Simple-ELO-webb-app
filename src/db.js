d5ae4ad3801dfd31454ee9b1bbe2ef25a3428c6c
Wilhelm Hansson <wille.live@gmail.com>
2020-04-19 22:23:45 +0200

    created mock data

---
+++ src/db.js
@@ -0,0 +1,39 @@
+const dogs = [
+    {
+        url: "https://i.ytimg.com/vi/PZpsigEdjOw/hqdefault.jpg",
+        id: 1,
+        score: 0
+    },
+    {
+        url: "https://i.ytimg.com/vi/3dcli9i_pvA/hqdefault.jpg",
+        id: 2,
+        score: 0
+    },
+    {
+        url: "https://i.ytimg.com/vi/Ra-Cq19vBXA/hqdefault.jpg",
+        id: 3,
+        score: 0
+    },
+    {
+        url: "https://i.ytimg.com/vi/2Twtsm6roa8/hqdefault.jpg",
+        id: 4,
+        score: 0
+    },
+    {
+        url: "https://www.surfcoast.vic.gov.au/files/assets/public/corporate-images/landing-page-images/doglookingup-800x531.jpg",
+        id: 5,
+        score: 0
+    },
+    {
+        url: "https://i.ytimg.com/vi/RVp3I7Eyjqg/hqdefault.jpg",
+        id: 6,
+        score: 0
+    },
+    {
+        url: "https://www.telegraph.co.uk/content/dam/Pets/spark/pets-at-home-2017/happy-dog-runs.jpg",
+        id: 7,
+        score: 0
+    }
+]
+
+export {dogs};
