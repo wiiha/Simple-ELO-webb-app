dc4f5e4f6cbf144e5a2790a6d30dcd4468c4b22c
Wilhelm Hansson <wille.live@gmail.com>
2020-04-20 20:53:41 +0200

    Created logic for randomizing a pair of dogs

--- src/App.svelte
+++ src/App.svelte
@@ -6,21 +6,55 @@
   import DisplayCard from "./DisplayCard.svelte";
 
   let competitors = dogs;
-  let testDog = dogs[0];
+  let dogI = {};
+  let dogII = {};
 
   console.log("Running");
   console.log("competitors", competitors);
-  
+  console.log("randomIntFromInterval", randomIntFromInterval(0, 7));
 
+  function setRandomDogs() {
+    const min = 0;
+    const max = competitors.length - 1;
+    console.log(min, max);
+    dogI = dogs[randomIntFromInterval(min, max)];
+    dogII = dogs[randomIntFromInterval(min, max)];
+    if (dogI.id === dogII.id) {
+      console.log("Same was choosen");
+
+      setRandomDogs();
+    }
+  }
+  setRandomDogs();
 </script>
+
+<!-- Navbar -->
+
+<!-- Main -->
+
+<!-- Potential footer -->
 
 <section class="section">
   <div class="container">
     <div class="columns">
       <div class="column">
-        <DisplayCard dog={testDog} />
+        <DisplayCard {...dogI} />
       </div>
-      <div class="column">Second column</div>
+      <div class="column">
+        <DisplayCard {...dogII} />
+      </div>
+
+    </div>
+    <div class="columns">
+      <div class="column">
+        <button class="button is-warning" on:click={setRandomDogs}>
+          New pair
+        </button>
+      </div>
+      <div class="column">
+      one: {dogI.id}
+      two: {dogII.id}
+      </div>
     </div>
   </div>
 </section>
